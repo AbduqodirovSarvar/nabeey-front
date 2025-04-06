e1a19339d19edfdd64f6fbcaba8fce029fd8e113
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2025-04-06 10:17:14 +0500

    add some changes

--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    // apiUrl: 'http://159.223.125.126:5000/api'
-    // apiUrl: 'https://localhost:7267/api'
-    apiUrl: 'https://api.kitobsevar.olami.uz/api'
-  };
